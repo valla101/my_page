642766ae2ebe2560108127c45c7db121a6ed16d9
Alex Valladares <alex.valla101@gmail.com>
2020-09-25 13:56:30 -0400

    added functionality for modal image enlargement

--- static/js/app.js
+++ static/js/app.js
@@ -11,5 +11,64 @@ function test (){
 
 gif_proj_1.on("click", test);
 
-// test (gif_proj_1, canvas_proj_1_gif);
 
+// Following is script for enlarging and closing images
+
+// Get the modal
+
+// Modal for NBA Project
+var modal = document.getElementById("myModal");
+
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    console.log("This worked");
+  };
+  
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+
+// Modal Function for Project 2
+
+// Modal for Power Plants Project
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+
+var span2 = document.getElementById("close_modal2");
+
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+  console.log("This worked");
+};
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
